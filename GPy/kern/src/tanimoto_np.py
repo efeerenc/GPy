c3fd8cdc56cc7273c00cbde939db019ed84ebf43
efeerenc <efeeren.c@gmail.com>
2023-03-30 14:57:09 +0300

    Update tanimoto_np.py

--- GPy/kern/src/tanimoto_np.py
+++ GPy/kern/src/tanimoto_np.py
@@ -9,10 +9,12 @@ from paramz.caching import Cache_this
 from paramz.transformations import Logexp
 from .kern import Kern
 
-class Tanimoto_NP():
+class Tanimoto_NP(Kern):
     
-    def __init__(self, input_dim, variance=1., name="tanimoto"):
-        self.variance=variance
+    def __init__(self, input_dim, variance=1., active_dims=None, name="tanimoto_NP"):
+        super(Tanimoto_NP, self).__init__(input_dim, active_dims, name)
+        self.variance = Param("variance", variance)
+        self.link_parameter(self.variance)
     
     def addition_elem_wise(self, a, b):
         add = a.reshape(-1, 1) + b.reshape(1, -1)
@@ -29,19 +31,14 @@ class Tanimoto_NP():
         Xs = np.square(np.linalg.norm(X, axis=1))
         X2s = np.square(np.linalg.norm(X2, axis=1))
         outer_product = X @ X2.T
-        print(Xs)
-        print(X2s)
-        print(outer_product)
         
         denominator = -outer_product + self.addition_elem_wise(Xs, X2s)
-        print(self.addition_elem_wise(Xs, X2s))
-        print(denominator)
         
         return self.variance * outer_product / denominator
     
-    def K_diag(self, X):
+    def Kdiag(self, X):
         return self.variance*np.ones(X.shape[0])
-
+    
     def parameters_changed(self):
         pass
 
